a672cb8119256aeda91404c829a4a2c1aeb208f0
aaronhuan <aaronhuang0000@gmail.com>
2025-04-30 09:42:59 -0400

    minor adjustments + add another test

--- tests/test_locations_model.py
+++ tests/test_locations_model.py
@@ -40,13 +40,11 @@ def location_zocca(session):
     return location
 
 def test_validate(location_london):
-    """Test the validate method on a valid location, should return nothing
-    """
+    """Test the validate method on a valid location, should return nothing"""
     location_london.validate()
 
 def test_validate_bad_data():
-    """Test the validate method on a invalid location, should raise Value Errors
-    """
+    """Test the validate method on a invalid location, should raise Value Errors"""
     bad = Locations(
         city_name="",
         latitude=100000,
@@ -58,31 +56,27 @@ def test_validate_bad_data():
 
 
 def test_get_location_by_id(location_london):
-    """ Tests the get_location_by_id on a correct id
-    """
+    """ Tests the get_location_by_id on a correct id"""
     loc = Locations.get_location_by_id(location_london.id)
     assert loc is not None
     assert loc.city_name=="London"
 
 
-def test_get_location_by_id_not_found(location_london):
-    """ Tests the get_location_by_id on an incorrect id 
-    """
+def test_get_location_by_id_not_found():
+    """ Tests the get_location_by_id on an incorrect id """
     with pytest.raises(ValueError):
         Locations.get_location_by_id(2039102012010121)
 
 
 def test_get_current_weather(app, location_zocca):
-    """ Tests the get_current_weather on a valid location 
-    """
+    """ Tests the get_current_weather on a valid location """
     loc= Locations.get_current_weather("Zocca", 44.34, 10.99)
     assert loc.city_name== "Zocca"
     assert loc.temp ==294.93
 
 
 def test_get_current_weather_not_found(app):
-    """Test error when compound key in get_current_weather method doesn't match any record
-    """
+    """Test error when compound key in get_current_weather method doesn't match any record"""
     with pytest.raises(ValueError):
         Locations.get_current_weather("mylittlepony", 0.0, 0.0)
 
@@ -111,3 +105,9 @@ def test_get_weather_history_returns_limit_3(session):
     assert isinstance(result, list)
     assert len(result) == 3
     assert result[0].time > result[1].time > result[2].time
+
+
+def test_get_weather_history_invalid():
+    """Test error when method get_weather_history_returns does not get a valid location."""
+    with pytest.raises(ValueError):
+        Locations.get_weather_history("oeeaeoeeeae", 12, 34)
